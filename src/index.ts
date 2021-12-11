068d9141b33948551f19a1fac0d753b72c5aceee
jiftechnify <jiftech.stlfy@gmail.com>
2021-12-11 15:46:04 +0900

    Fix: export TypedStorage from module root

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export * from './BaseStorage';
 export * from './Codec';
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('dev only output');
-  }
-  return a + b;
-};
+export * from './TypedStorage';
