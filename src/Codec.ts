fb063c2bfb14e9d485c6a5bbf7aacc1fd7bd8abb
jiftechnify <jiftech.stlfy@gmail.com>
2021-12-14 20:18:07 +0900

    Refactor: factor out common logics in Codecs that use JSON string as internal repr

--- src/Codec.ts
+++ src/Codec.ts
@@ -32,6 +32,26 @@ type BuiltinCodecsType = {
   jsonWithZod: <T>(zod: ZodType<T>) => Codec<T>;
 };
 
+// Codec for type `T` that encodes to/decodes from JSON string, with runtime validation on decoding.
+const jsonStrCodecWithValidation = <T>(validatorName: string, validate: (p: unknown) => T): Codec<T> => {
+  return {
+    encode: (t: T) => JSON.stringify(t),
+    decode: (s: string) => {
+      const parsed = JSON.parse(s) as unknown;
+      return validateParsedJSON(validatorName, validate, parsed);
+    },
+  };
+};
+
+// validate `parsed` with `validate`(validation logic). `validate` must throw error if validation is failed.
+const validateParsedJSON = <T>(validatorName: string, validate: (p: unknown) => T, parsed: unknown): T => {
+  try {
+    return validate(parsed);
+  } catch (e) {
+    throw new Error(`${validatorName} validation error: ${e}`);
+  }
+};
+
 export const codecs: BuiltinCodecsType = Object.freeze({
   /**
    * Codec for `string`.
@@ -110,10 +130,8 @@ export const codecs: BuiltinCodecsType = Object.freeze({
    * @param iots value of io-ts `Type<T>` that is used to validate value parsed from JSON.
    */
   jsonWithIoTs: <T>(iots: IoTsType<T>) => {
-    return Object.freeze({
-      encode: (t: T) => JSON.stringify(t),
-      decode: (s: string) => {
-        const parsed = JSON.parse(s) as unknown;
+    return Object.freeze(
+      jsonStrCodecWithValidation('io-ts', (parsed: unknown) => {
         const validated = iots.decode(parsed);
         return fold(
           // onLeft
@@ -123,8 +141,8 @@ export const codecs: BuiltinCodecsType = Object.freeze({
           // onRight
           (t: T) => t
         )(validated);
-      },
-    });
+      })
+    );
   },
   /**
    * Codec for type `T` that encodes to/decodes from JSON string, with validation on decoding powered by [superstruct](https://docs.superstructjs.org/).
@@ -132,18 +150,12 @@ export const codecs: BuiltinCodecsType = Object.freeze({
    * @param ss value of superstruct `Struct<T>` that is used to validate value parsed from JSON.
    */
   jsonWithSuperstruct: <T>(ss: SSStruct<T>) => {
-    return Object.freeze({
-      encode: (t: T) => JSON.stringify(t),
-      decode: (s: string) => {
-        const parsed = JSON.parse(s) as unknown;
-        try {
-          assertBySS(parsed, ss);
-          return parsed;
-        } catch (e) {
-          throw new Error(`superstruct validation error: ${e}`);
-        }
-      },
-    });
+    return Object.freeze(
+      jsonStrCodecWithValidation('superstruct', (parsed: unknown) => {
+        assertBySS(parsed, ss);
+        return parsed;
+      })
+    );
   },
   /**
    * Codec for type `T` that encodes to/decodes from JSON string, with validation on decoding powered by [zod](https://github.com/colinhacks/zod#readme).
@@ -151,16 +163,10 @@ export const codecs: BuiltinCodecsType = Object.freeze({
    * @param zod value of `ZodType<T>`("schema" object) that is used to validate value parsed from JSON.
    */
   jsonWithZod: <T>(zod: ZodType<T>) => {
-    return Object.freeze({
-      encode: (t: T) => JSON.stringify(t),
-      decode: (s: string) => {
-        const parsed = JSON.parse(s) as unknown;
-        try {
-          return zod.parse(parsed);
-        } catch (e) {
-          throw new Error(`zod validation error: ${e}`);
-        }
-      },
-    });
+    return Object.freeze(
+      jsonStrCodecWithValidation('zod', (parsed: unknown) => {
+        return zod.parse(parsed);
+      })
+    );
   },
 });
