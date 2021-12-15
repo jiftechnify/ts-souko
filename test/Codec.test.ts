5b4b9af9f3df253f53430db39be1258b47ab61e8
jiftechnify <jiftech.stlfy@gmail.com>
2021-12-15 22:45:00 +0900

    Add: test for codecs.bigint, fix unexpected nesting test

--- test/Codec.test.ts
+++ test/Codec.test.ts
@@ -47,50 +47,68 @@ describe('codecs.number', () => {
       }).toThrow();
     }
   });
+});
 
-  describe('codecs.boolean', () => {
-    test('encode-then-decode preserves value', () => {
-      const cases = [true, false];
-      for (const c of cases) {
-        const encoded = codecs.boolean.encode(c);
-        expect(codecs.boolean.decode(encoded)).toEqual(c);
-      }
-    });
-    test('throws error if decode a string not parsable as boolean', () => {
-      const cases = ['1', '0', 'hoge', 'yes', 'no', '[]', '{}', 'null', 'undefined'];
-      for (const c of cases) {
-        expect(() => {
-          codecs.boolean.decode(c);
-        }).toThrow();
-      }
-    });
+describe('codecs.bigint', () => {
+  test('encode-then-decode preserves value', () => {
+    const cases = [BigInt(1), BigInt(0), BigInt(-1), BigInt('9007199254740992'), BigInt('9007199254740992')];
+    for (const c of cases) {
+      const encoded = codecs.bigint.encode(c);
+      expect(codecs.bigint.decode(encoded)).toEqual(c);
+    }
   });
-
-  describe('codecs.arrayOf', () => {
-    test('encode-then-decode preserves array of number', () => {
-      const numArrayCodec = codecs.arrayOf(codecs.number);
-      const arr = [1, -1, 0, -0, Number.MAX_SAFE_INTEGER, 123.4, 0.1 + 0.2, Number.POSITIVE_INFINITY];
-
-      const encoded = numArrayCodec.encode(arr);
-      expect(numArrayCodec.decode(encoded)).toEqual(arr);
-    });
-    test('throws error when decoding a string not parsable as array', () => {
-      const arrayCodec = codecs.arrayOf(codecs.string);
-      const cases = ['str', '1', 'true', '{"a":1,"b":2}', ':not-a-json:', 'null'];
-
-      for (const c of cases) {
-        expect(() => {
-          arrayCodec.decode(c);
-        }).toThrow('is not decodable as array');
-      }
-    });
-    test('throws error when decoding a string parsable as array but some element is not decodable by inner codec', () => {
-      const boolArrayCodec = codecs.arrayOf(codecs.boolean);
-      const input = '["true", "false", "1"]';
-
+  test('throws error if decode a string not parsable as bigint', () => {
+    const cases = ['1n', '0.0', 'true', 'false', '[]', '{}', 'null', 'undefined'];
+    for (const c of cases) {
       expect(() => {
-        boolArrayCodec.decode(input);
-      }).toThrow('is not decodable as array of specified type');
-    });
+        codecs.bigint.decode(c);
+      }).toThrow();
+    }
+  });
+});
+
+describe('codecs.boolean', () => {
+  test('encode-then-decode preserves value', () => {
+    const cases = [true, false];
+    for (const c of cases) {
+      const encoded = codecs.boolean.encode(c);
+      expect(codecs.boolean.decode(encoded)).toEqual(c);
+    }
+  });
+  test('throws error if decode a string not parsable as boolean', () => {
+    const cases = ['1', '0', 'hoge', 'yes', 'no', '[]', '{}', 'null', 'undefined'];
+    for (const c of cases) {
+      expect(() => {
+        codecs.boolean.decode(c);
+      }).toThrow();
+    }
+  });
+});
+
+describe('codecs.arrayOf', () => {
+  test('encode-then-decode preserves array of number', () => {
+    const numArrayCodec = codecs.arrayOf(codecs.number);
+    const arr = [1, -1, 0, -0, Number.MAX_SAFE_INTEGER, 123.4, 0.1 + 0.2, Number.POSITIVE_INFINITY];
+
+    const encoded = numArrayCodec.encode(arr);
+    expect(numArrayCodec.decode(encoded)).toEqual(arr);
+  });
+  test('throws error when decoding a string not parsable as array', () => {
+    const arrayCodec = codecs.arrayOf(codecs.string);
+    const cases = ['str', '1', 'true', '{"a":1,"b":2}', ':not-a-json:', 'null'];
+
+    for (const c of cases) {
+      expect(() => {
+        arrayCodec.decode(c);
+      }).toThrow('is not decodable as array');
+    }
+  });
+  test('throws error when decoding a string parsable as array but some element is not decodable by inner codec', () => {
+    const boolArrayCodec = codecs.arrayOf(codecs.boolean);
+    const input = '["true", "false", "1"]';
+
+    expect(() => {
+      boolArrayCodec.decode(input);
+    }).toThrow('is not decodable as array of specified type');
   });
 });
